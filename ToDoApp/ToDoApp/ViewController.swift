e5d2e96e0b1890259fa17a3ecbcb4f7b57a0311d
William Ching <willykamus@gmail.com>
2020-04-05 17:02:21 -0400

    Initial Commit

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by William Ching on 2020-04-05.
+//  Copyright © 2020 William Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
